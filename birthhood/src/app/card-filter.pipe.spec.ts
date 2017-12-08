aba0f10cdfeb72d5953b9b3f56194249ad9a711c
t1brunne <t1brunne@hsr.ch>
2017-12-08 10:05:18 +0100

    files löschen

--- birthhood/src/app/card-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CardFilterPipe } from './card-filter.pipe';
-
-describe('CardFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CardFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
