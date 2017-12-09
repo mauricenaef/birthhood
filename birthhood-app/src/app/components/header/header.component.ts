c332a4209b983bd42cb9add42dcf01253d349f26
t1brunne <t1brunne@hsr.ch>
2017-12-09 13:54:45 +0100

    logout added

--- birthhood-app/src/app/components/header/header.component.ts
+++ birthhood-app/src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  logout(){
+    this.authService.logout();
+    this.router.navigateByUrl('/');
+  }
 }
