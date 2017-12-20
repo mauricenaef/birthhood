cd8d49b57e55c6f233fc9b407fcdcebec06ae108
Tobias Brunner <tobias.brunner@zuerich.ch>
2017-12-20 10:17:53 +0100

    angularfirestore als provider? => nein

--- birthhood-app/src/app/app.module.ts
+++ birthhood-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
 import { AngularFireModule, } from 'angularfire2';
-import { AngularFirestoreModule, } from 'angularfire2/firestore';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
