3c218edd102a7aaab47177af98d3efc8cacaef05
t1brunne <t1brunne@hsr.ch>
2017-12-28 16:56:09 +0100

    added redirect

--- birthhood-app/src/app/modules/experience/components/user-experience-list/user-experience-list.component.ts
+++ birthhood-app/src/app/modules/experience/components/user-experience-list/user-experience-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ExperienceService } from '../../services/experience.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-user-experience-list',
   templateUrl: './user-experience-list.component.html',
@@ -11,18 +13,25 @@ export class UserExperienceListComponent implements OnInit {
 
   experienceList;
 
-  constructor( private af: AngularFireAuth, private experienceService: ExperienceService  ) { }
+  constructor(private af: AngularFireAuth, private experienceService: ExperienceService, private router: Router) { }
 
   ngOnInit() {
     /*let userId = this.authService.currentUserId;
     console.log("userexperience " + userId);
 */
-  this.af.auth.onAuthStateChanged(
-      currentUser => 
-    this.experienceService.getExperiencesByUserId(currentUser.uid).subscribe(
-      x => this.experienceList = x
-    ));
-    
+    this.af.auth.onAuthStateChanged(
+      currentUser => {
+        if (currentUser) {
+          this.experienceService.getExperiencesByUserId(currentUser.uid).subscribe(
+            x => this.experienceList = x
+          )
+        } else {
+          //ev. gar nicht nötig, da über ROuteGuard gekapselt wird
+    this.router.navigateByUrl('/');
+        }
+      }
+    );
+
 
   }
 
