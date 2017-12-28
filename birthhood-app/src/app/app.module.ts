563a69984e6a7cd402ea11e4e064a5db6526f322
t1brunne <t1brunne@hsr.ch>
2017-12-28 15:20:00 +0100

    removed UI RouterModule

--- birthhood-app/src/app/app.module.ts
+++ birthhood-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { UIRouterModule } from "@uirouter/angular";
+//import { UIRouterModule } from "@uirouter/angular";
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
