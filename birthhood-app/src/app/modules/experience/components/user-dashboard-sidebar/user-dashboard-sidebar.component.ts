f02fb2ea45b89e79f62002120db59e5ce3cf30fc
t1brunne <t1brunne@hsr.ch>
2018-01-02 10:50:02 +0100

    user-dashboard-sidebar markup

--- birthhood-app/src/app/modules/experience/components/user-dashboard-sidebar/user-dashboard-sidebar.component.ts
+++ birthhood-app/src/app/modules/experience/components/user-dashboard-sidebar/user-dashboard-sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-dashboard-sidebar',
@@ -11,12 +12,22 @@ export class UserDashboardSidebarComponent implements OnInit {
 
   currentUser;
 
-  constructor( private af: AngularFireAuth ) { }
+  constructor( private af: AngularFireAuth , private router: Router) { }
 
 
   ngOnInit() {
     this.af.auth.onAuthStateChanged(
-       x => this.currentUser = x );
+       x => {
+         this.currentUser = x ;
+         console.log(x);
+       }
+        )
   }
 
+
+  logout(){
+    this.af.auth.signOut();
+
+    this.router.navigateByUrl('/');
+  }
 }
