6453686362d4e50973314a1c64d943b6af0a3270
t1brunne <t1brunne@hsr.ch>
2018-01-02 20:39:56 +0100

    toastr added

--- birthhood-app/src/app/components/login/login.component.ts
+++ birthhood-app/src/app/components/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 import { AngularFireAuth } from 'angularfire2/auth';
 import { EmailPasswordCredentials } from '../../models/email-password-credentials';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -11,42 +11,26 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  error: any;
+
   email: string;
   password: string;
 
-  constructor(private af: AngularFireAuth, private router: Router) { }
+  constructor(private af: AngularFireAuth, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
-
   onSubmit(formData) {
     if (formData.valid) {
-      console.log(formData.value);
       this.af.auth.signInWithEmailAndPassword(formData.value.email,
         formData.value.password).then(x => {
-          console.log("eingeloggt");
           this.router.navigate(['/user-dashboard']);
-
+          this.toastr.success(`Mit ${formData.value.email} eingeloggt`, "Login");
         }
-        );
-      /*this.af.auth.login({
-        email: formData.value.email,
-        password: formData.value.password
-      },
-      {
-        provider: AuthProviders.Password,
-        method: AuthMethods.Password,
-      }).then(
-        (success) => {
-        console.log(success);
-        this.router.navigate(['/members']);
-      }).catch(
-        (err) => {
-        console.log(err);
-        this.error = err;
-      })*/
+        ).catch(err => {
+          this.toastr.error(err, "Fehler bei Login");
+        })
+        ;
     }
   }
 }
