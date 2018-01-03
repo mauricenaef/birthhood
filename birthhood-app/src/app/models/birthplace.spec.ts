a009a216ef39c353fb9e13974ed66a84c41a595f
t1brunne <t1brunne@hsr.ch>
2018-01-03 16:21:01 +0100

    birthplace test updated

--- birthhood-app/src/app/models/birthplace.spec.ts
+++ birthhood-app/src/app/models/birthplace.spec.ts
@@ -17,7 +17,9 @@ fdescribe('Class: Birthplace', () => {
   });
 
   it('should calculate the correct distance', () => { 
-    let distance = birthplace.distance(<LatLngLiteral>{lat:45, lng:7})
+    let distance = birthplace.distance(<LatLngLiteral>{lat:45, lng:7});
+    let distance2 = birthplace.distance(<LatLngLiteral>{lat:45, lng:8});
     expect(distance).toBe(0); 
+    expect(distance2).toBe(78.62618767687454);
   });
 });
