2bb71f06f1f4498f5d49596a876c6fb067b13932
Marcel Naef <me@mauricenaef.ch>
2018-01-03 22:47:02 +0100

    selected wrong DB

--- birthhood-app/src/environments/environment.ts
+++ birthhood-app/src/environments/environment.ts
@@ -5,21 +5,21 @@
 
 export const environment = {
   production: false,
-  firebase: {
+  /* firebase: {
     apiKey: "AIzaSyCpXT0NeCCpYWeL2vITlDbYmQ6HGekgDqI",
     authDomain: "birthhood2.firebaseapp.com",
     databaseURL: "https://birthhood2.firebaseio.com",
     projectId: "birthhood2",
     storageBucket: "",
     messagingSenderId: "836576784093"
-  }
-  /* firebase: {
+  } */
+  firebase: {
     apiKey: "AIzaSyBo-NplVsfsCeD_m_kZ_6Y8BzNnVKTHbIo",
     authDomain: "birthhood.firebaseapp.com",
     databaseURL: "https://birthhood.firebaseio.com",
     projectId: "birthhood",
     storageBucket: "birthhood.appspot.com",
     messagingSenderId: "986661546141"
-  } */
+  }
 
 };
