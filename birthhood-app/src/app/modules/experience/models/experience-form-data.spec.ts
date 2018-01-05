1aec4f24a04875307ffe44bdcc0917e6d20a04ad
t1brunne <t1brunne@hsr.ch>
2018-01-05 16:14:47 +0100

    added unit test

--- birthhood-app/src/app/modules/experience/models/experience-form-data.spec.ts
+++ birthhood-app/src/app/modules/experience/models/experience-form-data.spec.ts
@@ -4,10 +4,26 @@ describe('Class: Experience', () => {
     let experience: Experience;
 
 
-  it('should calculate the correct distance', () => { 
+  it('should clear values', () => { 
     experience = Object.create(Experience.prototype);
     experience.birth_name = "test";
     experience.clear();
     expect(experience.birth_name).toBe(''); 
+  });
+
+  it('should calculate average score correctly', () => { 
+    experience = Object.create(Experience.prototype);
+    experience.w1 = 1;
+    experience.w2 = 2;
+    experience.w3 = 3;
+    experience.w4 = 4;
+    experience.w5 = 5;
+    expect(experience.score_w).toBe(3); 
+  });
+
+  it('should calculate average score with null values correctly', () => { 
+    experience = Object.create(Experience.prototype);
+    experience.m1 = 1;
+    expect(experience.score_m).toBe(0.2); 
   });
 });
