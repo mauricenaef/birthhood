1c0e9a25fd125147d8cd4c16dc47e10c81a5ee63
t1brunne <t1brunne@hsr.ch>
2018-01-05 10:59:20 +0100

    moved mapstyles to config-json

--- birthhood-app/src/app/components/birthplaces-map/birthplaces-map.component.ts
+++ birthhood-app/src/app/components/birthplaces-map/birthplaces-map.component.ts
@@ -11,6 +11,7 @@ import { LatLngBounds } from '@agm/core/services/google-maps-types';
 import { NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import * as mapstyles from '../../../assets/config/map-styles.json';
 declare var google: any;
 
 @Component({
@@ -22,212 +23,19 @@ declare var google: any;
 export class BirthplacesMapComponent implements OnInit, OnDestroy {
 
   // Custom Map Styles
-  public styles = [
-    {
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#f5f5f5"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#616161"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.text.stroke",
-      "stylers": [
-        {
-          "color": "#f5f5f5"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.land_parcel",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.land_parcel",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#bdbdbd"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.neighborhood",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#eeeeee"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#757575"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#e5e5e5"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#ffffff"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "labels",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#757575"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#dadada"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#616161"
-        }
-      ]
-    },
-    {
-      "featureType": "road.local",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.line",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#e5e5e5"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.station",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#eeeeee"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#c9c9c9"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    }
-  ];
+  public styles;
 
   isDetail: boolean = false;
 
   @ViewChild(AgmMap) private map: any;
   items$: Observable<any[]>;
 
-
   latLng: LatLngLiteral;
   zoomOutNumber: number = 3;
   bounds: LatLngBounds;
 
   routersubscription: Subscription;
+  
   constructor(public birthplaceService: BirthplaceService, public router: Router) {
     /*fallback-location. HSR?*/
     this.latLng = <LatLngLiteral>{
@@ -235,6 +43,7 @@ export class BirthplacesMapComponent implements OnInit, OnDestroy {
       lng: 8.6
     };
 
+    this.styles = mapstyles;
 
     this.items$ = birthplaceService.getBirhplacesOnMap();
 
@@ -323,7 +132,7 @@ export class BirthplacesMapComponent implements OnInit, OnDestroy {
             bounds.extend(latLngObject);
           }
         );
-        
+
         this.map.triggerResize()
           .then(() => {
             this.map._mapsWrapper.fitBounds(bounds);
