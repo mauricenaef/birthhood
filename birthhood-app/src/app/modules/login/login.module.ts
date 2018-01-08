3878ae927d783924842619eb6d9fb03a683fb941
t1brunne <t1brunne@hsr.ch>
2018-01-08 17:30:57 +0100

    routerlink for signup fixed

--- birthhood-app/src/app/modules/login/login.module.ts
+++ birthhood-app/src/app/modules/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './../../app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AuthService } from './services/auth.service';
@@ -10,7 +11,8 @@ import { AngularFireAuth } from 'angularfire2/auth';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     LoginComponent,
