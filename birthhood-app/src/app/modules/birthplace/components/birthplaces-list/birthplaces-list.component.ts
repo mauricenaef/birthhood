6a88f0151f6785980fca902e6ef50d3c14bc8927
t1brunne <t1brunne@hsr.ch>
2018-01-16 22:13:19 +0100

    changes

--- birthhood-app/src/app/modules/birthplace/components/birthplaces-list/birthplaces-list.component.ts
+++ birthhood-app/src/app/modules/birthplace/components/birthplaces-list/birthplaces-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit , OnDestroy } from '@angular/core';
 import { BirthplaceService } from '../../services/birthplace.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
